a2e147829e0c2dc202ddefb2c8d37925435becc8
Andrey <ernyhov.andrey13@yandex.ru>
2022-05-03 17:21:13 +0500

    3 commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Задание 1")
 k = int(input("Введите левую границу: "))
-m = int(input("Введите правую границу: "))
+m = int(input("Введите границу: "))
 sum = 0
 for i in range(k, m + 1):
      sum += pow(i,2)
